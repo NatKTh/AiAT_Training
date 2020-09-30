8801f7309a6128f5346376d25c01665e0cae2367
NatKTh <nat_k1@yahoo.com>
2020-09-30 21:33:46 +0700

    New Start

---
+++ TestVersion.py
@@ -0,0 +1,15 @@
+#%%
+import tensorflow as tf
+import torch
+import numpy
+import matplotlib
+import pandas
+import cv2
+
+print("Tensorflow = ",tf.__version__)
+print("PyTorch = ",torch.__version__)
+print("Numpy = ",numpy.__version__)
+print("Matplotlib = ",matplotlib.__version__)
+print("Pandas = ",pandas.__version__)
+print("OpenCV = ",cv2.__version__)
+# %%
