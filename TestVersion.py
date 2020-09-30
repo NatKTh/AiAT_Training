20eb16b4055656483953d11af5a1ba5ed926df48
NatKTh <nat_k1@yahoo.com>
2020-09-30 21:37:02 +0700

    Correct import file

--- TestVersion.py
+++ TestVersion.py
@@ -1,15 +1,17 @@
 #%%
 import tensorflow as tf
 import torch
-import numpy
+import numpy as np
 import matplotlib
-import pandas
+import pandas as pd
+import seaborn as sns
 import cv2
 
 print("Tensorflow = ",tf.__version__)
 print("PyTorch = ",torch.__version__)
-print("Numpy = ",numpy.__version__)
+print("Numpy = ",np.__version__)
 print("Matplotlib = ",matplotlib.__version__)
-print("Pandas = ",pandas.__version__)
+print("Pandas = ",pd.__version__)
+print("Seaborn = ",sns.__version__)
 print("OpenCV = ",cv2.__version__)
 # %%
